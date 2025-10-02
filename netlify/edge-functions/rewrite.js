50a877436f2a0787a874c65f6ae7f3979ef3ee66
MasterDev <juanpb.96@gmail.com>
2025-10-02 11:23:40 -0500

    Include user geo data in custom header

--- netlify/edge-functions/rewrite.js
+++ netlify/edge-functions/rewrite.js
@@ -1,11 +1,17 @@
 const WEATHER_APP_URL =
   "https://juanbonilla-fem-weather-app.netlify.app/FEM_weather-app";
 
-export default async (_, context) => {
+export default async (request, context) => {
   try {
-    const response = await fetch(WEATHER_APP_URL);
+    const headers = new Headers(request.headers);
+    headers.set("x-geo-context", context.geo);
 
-    return new Response(response.body, { status: response.status, headers });
+    const proxyRequest = new Request(WEATHER_APP_URL, {
+      ...request,
+      headers,
+    });
+
+    return await fetch(proxyRequest);
   } catch (error) {
     context.log("Error fetching external resource:", error);
     return new Response("Error fetching external content", { status: 500 });
